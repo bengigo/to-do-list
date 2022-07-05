884aa8ac1b3baf0dbdc40ea9adf91c3b9a0cc043
bengigenc <genc.bengi@gmail.com>
2022-07-05 21:28:14 +0300

    Create sample array of tasks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this scripts
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const todos = [
+    {
+        description: 'Finish list structure',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'Load dishes',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'Put the soup to fridge',
+        completed: false,
+        index: 4,
+    },
+    {
+        description: 'Check mails',
+        completed: false,
+        index: 3,
+    },
+    {
+        description: 'Do yoga',
+        completed: false,
+        index: 2,
+    }
+]
