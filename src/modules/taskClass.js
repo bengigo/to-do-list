52e3241e29df492802dc932a854649fc8b1ed16e
bengigenc <genc.bengi@gmail.com>
2022-07-06 23:42:09 +0300

    Set index values and display task list

--- src/modules/taskClass.js
+++ src/modules/taskClass.js
@@ -11,45 +11,50 @@ export default class Task {
   static add() {
     const taskInput = document.querySelector("#task-input");
     const addIcon = document.querySelector("#add");
-    const messageBox = document.querySelector('#message-display');
-    messageBox.style.display = 'none';
+    const messageBox = document.querySelector("#message-display");
+    messageBox.style.display = "none";
 
     addIcon.addEventListener("click", () => {
-      if (taskInput.value !== '') {
+      let toDos = [];
+      toDos = JSON.parse(localStorage.getItem("toDos") || "[]");
+      if (taskInput.value !== "") {
         const newTask = new Task(taskInput.value);
         toDos.push(newTask);
-        console.log(toDos);
-        localStorage.setItem('toDos', JSON.stringify(toDos));
-        taskInput.value = '';
-        messageBox.style.display = 'flex';
-        messageBox.innerText = 'Added';
-        messageBox.style.color = 'green';
+        toDos.forEach((obj, i) => {
+          obj.index = i + 1;
+        });
+        localStorage.setItem("toDos", JSON.stringify(toDos));
+        taskInput.value = "";
+        window.location.reload();
       } else {
-        messageBox.style.display = 'flex';
-        messageBox.innerText = 'You didn\'t write anything!';
-        messageBox.style.color = 'tomato';
+        messageBox.style.display = "flex";
+        messageBox.innerText = "You didn't write anything!";
+        messageBox.style.color = "tomato";
       }
+      console.log('hey');
+      console.log(toDos);
+
     });
   }
 
   static display() {
     const listContainer = document.querySelector("#list");
-
-    const orderedTask = toDos.sort((a, b) => a.index - b.index);
-
-    orderedTask.forEach((task, index) => {
-      listContainer.innerHTML += `
-            <li>
-              <div class="task-info">
-                <input type="checkbox" id="${orderedTask[index].index}">
-                <label for="${orderedTask[index].index}">${orderedTask[index].description}</label>
-              </div>
-              <span class="material-symbols-outlined">more_vert</span>
-            </li>
-        `;
-    });
+    let toDos = JSON.parse(localStorage.getItem("toDos") || "[]");
+    console.log(toDos);
+    if (toDos.length > 0) {
+      toDos.forEach((task) => {
+        listContainer.innerHTML += `
+                        <li>
+                          <div class="task-info">
+                            <input type="checkbox" id="${task.index}">
+                            <label for="${task.index}">${task.description}</label>
+                          </div>
+                          <span class="material-symbols-outlined">more_vert</span>
+                        </li>
+                    `;
+      });
+    }
   }
-  //   check display function again
 
   static remove() {}
 }
