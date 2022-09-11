dcaf54def5ffc225ddcc84e7af4bce1d84f2468c
bengigenc <genc.bengi@gmail.com>
2022-09-11 22:35:10 +0300

    Fix checkbox display after page reload

--- src/modules/interactions.js
+++ src/modules/interactions.js
@@ -1,14 +1,18 @@
 export default function interactions() {
   const checkboxes = document.querySelectorAll('.checkbox');
   checkboxes.forEach((box) => {
-    box.addEventListener('change', (e) => {
+    box.addEventListener('click', (e) => {
       let toDos = [];
       toDos = JSON.parse(localStorage.getItem('toDos') || '[]');
 
       toDos.forEach((obj) => {
-        if (e.target.classList.contains(obj.index)) {
+        if ((e.target.id === `check${obj.index}`) && (obj.completed === false)) {
           obj.completed = true;
           e.target.checked = true;
+          localStorage.setItem('toDos', JSON.stringify(toDos));
+        } else if ((e.target.id === `check${obj.index}`) && (obj.completed === true)) {
+          obj.completed = false;
+          e.target.checked = false;
           localStorage.setItem('toDos', JSON.stringify(toDos));
         }
       });
